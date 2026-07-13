faf40610fb47949d862220b72059a2e4d23379be
Ilya Kz <i.kozlov@onpoint.ru>
2026-07-13 17:29:32 +0300

    add button

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <button class="custom-button">
+    <slot/>
+  </button>
+</template>
+
+<style scoped>
+.custom-button {
+  background-color: var(--button-color);
+  color: var(--font-color);
+  font-family: var(--font);
+  font-size: 24px;
+  border: none;
+  padding: 16px 10px;
+  cursor: pointer;
+  border-radius: 50px;
+  min-width: 335px;
+}
+</style>
